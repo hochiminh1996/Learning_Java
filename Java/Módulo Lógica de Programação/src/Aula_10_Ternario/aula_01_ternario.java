50e2a627e5fa027abe03fd8ece9c6cccfb7d574f
hochiminh1996 <ambrsp@hotmail.com>
2023-03-25 16:15:24 -0300

    Switch

---
+++ Java/Módulo Lógica de Programação/src/Aula_10_Ternario/aula_01_ternario.java
@@ -0,0 +1,17 @@
+package Aula_10_Ternario;
+
+public class aula_01_ternario {
+
+	public static void main(String[] args) {
+		
+		int age = 10;
+		String txt;
+		
+		txt = age>=18 ? "Maior de idade" : "Menor de idade";
+		
+		System.out.println(txt);
+	
+
+	}
+
+}
---
+++ Java/Módulo Lógica de Programação/src/Aula_10_Ternario/aula_11_ternario2.java
@@ -0,0 +1,15 @@
+package Aula_10_Ternario;
+
+public class aula_11_ternario2 {
+
+	public static void main(String[] args) {
+		double preco=34.5, valor;
+		
+		valor = (preco<20) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.println(valor);
+		
+
+	}
+
+}
