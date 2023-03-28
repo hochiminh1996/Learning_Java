ca4e68e6cc43ca93908f6cd5ac4dbdf3c44f47a4
hochiminh1996 <ambrsp@hotmail.com>
2023-03-28 17:53:49 -0300

    Estrutura de controle de fluxo : for

---
+++ Java/Módulo Lógica de Programação/src/Aula_12_for/Aula_01_for.java
@@ -0,0 +1,20 @@
+package Aula_12_for;
+
+public class Aula_01_for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Usamos for quando sabemos exatamente a quantidade de repetições
+		 que será efetuada
+		 
+		 Composição -> contador, condição e incrementador
+		 
+		 */
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
