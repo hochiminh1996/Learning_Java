7a82703ce718dc90788eb3b6990abdc04e765c70
hochiminh1996 <ambrsp@hotmail.com>
2023-06-26 23:23:55 -0300

    Polimorfismo e herança

---
+++ Java/Módulo Herança e Polimorfismo/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		/*
+		 * Herança é uma dos pilares da programação orientada a objetos
+		 * É um tipo de associação entre classes que permite que uma classe herde todos os métodos
+		 * e atributos de outra
+		 * 
+		 * 
+		 * Vantagens :
+		 * Reuso 
+		 * Polimorfismo -> Um mesmo método que executa funções diferentes
+		 * */
+		System.out.println("Olá, mundo");
+	}
+}
---
+++ Java/Módulo Herança e Polimorfismo/src/entities/BusinessAccount.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class BusinessAccount extends Account {
+
+	private double loan_limit;
+
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loan_limit) {
+		super(number, holder, balance);
+		this.loan_limit = loan_limit;
+	}
+
+	public double getLoan_limit() {
+		return loan_limit;
+	}
+
+	public void setLoan_limit(double loan_limit) {
+		this.loan_limit = loan_limit;
+	}
+
+	public void loan(Double amount) {// empréstimo
+		if (amount <= loan_limit) {
+			balance+=amount - 10;
+		}else {
+			System.out.println("Limite indisponível");
+		}
+	}
+
+}
